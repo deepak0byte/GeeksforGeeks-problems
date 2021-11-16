2f2b6d5f3e0c72de620ffbeb30fa624cb6779dab
Deepak Kumar <86466798+deepak0byte@users.noreply.github.com>
2021-11-16 10:40:07 +0530

    Create Shortest Common Supersequence.cpp

---
+++ DP/Shortest Common Supersequence.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    //Function to find length of shortest common supersequence of two strings.
+    int shortestCommonSupersequence(string X, string Y, int m, int n)
+    {
+        //code here
+        int dp[m + 1][n + 1];
+        memset(dp, 0, sizeof(dp));
+        for (int i = 1; i < m + 1; i++) {
+            for (int j = 1; j < n + 1; j++) {
+                if (X[i - 1] == Y[j - 1]) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                }
+                else {
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        int d = m - dp[m][n];
+        int x = n - dp[m][n];
+        return dp[m][n] + d + x;
+    }
+};
+int main()
+{
+
+    int t;
+
+    //taking total testcases
+    cin >> t;
+    while (t--) {
+        string X, Y;
+        //taking String X and Y
+        cin >> X >> Y;
+
+        //calling function shortestCommonSupersequence()
+        Solution obj;
+        cout << obj.shortestCommonSupersequence(X, Y, X.size(), Y.size()) << endl;
+    }
+    return 0;
+}
