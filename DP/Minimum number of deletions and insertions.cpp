46143e07cd772627c9ff11a49572fc2fec4a1eca
Deepak Kumar <86466798+deepak0byte@users.noreply.github.com>
2021-11-16 12:09:31 +0530

    Create Minimum number of deletions and insertions.cpp

---
+++ DP/Minimum number of deletions and insertions.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int minOperations(string str1, string str2)
+    {
+        // Your code goes here
+        int n = str2.length();
+        int m = str1.length();
+        int dp[m + 1][n + 1];
+        memset(dp, 0, sizeof(dp));
+        for (int i = 1; i < m + 1; i++) {
+            for (int j = 1; j < n + 1; j++) {
+                if (str1[i - 1] == str2[j - 1]) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                }
+                else {
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        int ans = m - dp[m][n];
+        ans += n - dp[m][n];
+        return ans;
+
+    }
+};
+
+// { Driver Code Starts.
+int main()
+{
+
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string s1, s2;
+        cin >> s1 >> s2;
+
+        Solution ob;
+        cout << ob.minOperations(s1, s2) << "\n";
+
+    }
+    return 0;
+}
